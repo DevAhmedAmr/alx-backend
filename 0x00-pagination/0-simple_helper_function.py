e91f0177d10e56f592b6ceabdc961cde3249a22e
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-22 14:38:50 +0300

    0. Simple helper function (automated command)

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -13,7 +13,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
 
 if "__main__" == __name__:
-    res = index_range(page=1, page_size=10)
+    res = index_range(page=1, page_size=3)
     print(type(res))
     print(res)
 # 1
