49e6e02513c08545de0ce4dc7a0d4dffb370ae5b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-22 14:33:15 +0300

    0. Simple helper function (automated command)

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,3 +10,11 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Retrieves the index range from a given page and page size.
     """
     return ((page_size * page) - page_size, (page_size * page))
+
+
+if "__main__" == __name__:
+    res = index_range(page=1, page_size=10)
+    print(type(res))
+    print(res)
+# 1
+# >>> 0 10
