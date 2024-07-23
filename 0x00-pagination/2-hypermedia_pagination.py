8104a5527d64931efe91193409dc45e90a78a503
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-23 05:33:17 +0300

    Improve docstring formatting in hypermedia_pagination.py
    Enhanced the readability of the docstring by adding line breaks and
    reformatting the text. This ensures that the documentation is clearer
    and more consistent, especially for the 'prev_page' and 'total_pages'
    descriptions.

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -49,11 +49,18 @@ class Server:
         """ returns a dictionary containing the following key-value pairs:
 
             >>>> page_size: the length of the returned dataset page
+
             >>>> page: the current page number
-            >>>> data: the dataset page (equivalent to return from previous task)
+
+            >>>> data: the dataset page
+
             >>>> next_page: number of the next page, None if no next page
-            >>>> prev_page: number of the previous page, None if no previous page
-            >>>> total_pages: the total number of pages in the dataset as an integer
+
+            >>>> prev_page: number of the previous page,
+                 None if no previous page
+
+            >>>> total_pages: the total number of pages in the dataset as
+                 an integer
         """
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
