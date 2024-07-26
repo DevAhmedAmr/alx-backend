70372f7b80420652b55f7d3445a62aa79a78e75a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-26 14:59:10 +0300

    Implement LRU Cache algorithm with Python
    This commit implements the LRU (Least Recently Used) Cache algorithm in Python. The `LRUCache` class inherits from `BaseCaching` and is a caching system that stores a limited number of items. The cache uses an ordered dictionary to keep track of the items and their order of use. The `put` method adds an item to the cache, while the `get` method retrieves an item by key. The commit also includes helper functions to decrement the points and remove the least recently used item from the cache. Tests were included in the `if __name__ == "__main__"` block.

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -7,6 +7,14 @@ from collections import OrderedDict
 from base_caching import BaseCaching
 
 
+def get_key_from_value(d: dict, target_value: any):
+    for key, value in d.items():
+        if value == target_value:
+            return key
+
+    return None
+
+
 class LRUCache(BaseCaching):
     '''A class `LRUCache` that inherits from
        `BaseCaching` and is a caching system
@@ -17,24 +25,115 @@ class LRUCache(BaseCaching):
         '''
         super().__init__()
         self.cache_data = OrderedDict()
+        self.points = OrderedDict()
+        self.mini = self.MAX_ITEMS
 
     def put(self, key, item):
         """Adds an item in the cache.
         """
-        if key is None or item is None:
-            return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", lru_key)
+        # {1,0,2,3}
+        if len(self.cache_data) < self.MAX_ITEMS:
             self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=False)
+
+            self.points = self.decrement(self.points, key)
+
+            self.points[self.MAX_ITEMS] = key
+
+        elif len(self.cache_data) == self.MAX_ITEMS and key in self.cache_data:
+            self.points = self.decrement2(self.points, key)
         else:
-            self.cache_data[key] = item
+
+            oldest_key = self.points[self.mini]
+            del self.points[self.mini]
+            print("DISCARD:", oldest_key)
+            del self.cache_data[oldest_key]
+
+            self.points = self.decrement(self.points, key)
+            self.points[self.MAX_ITEMS] = key
+        self.cache_data[key] = item
 
     def get(self, key):
         """Retrieves an item by key.
         """
-        if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
-        return self.cache_data.get(key, None)
+        if key not in self.cache_data:
+            return None
+
+        if len(self.cache_data) < self.MAX_ITEMS:
+            value = self.cache_data.get(key)
+
+            self.points = self.decrement(self.points, key)
+            self.points[self.MAX_ITEMS] = key
+            return value
+
+        elif len(self.cache_data) == self.MAX_ITEMS and key in self.cache_data:
+            self.points = self.decrement2(self.points, key)
+            return self.cache_data.get(key)
+        else:
+
+            oldest_key = self.points[self.mini]
+
+            del self.points[self.mini]
+            print("DISCARD:", oldest_key)
+            del self.cache_data[oldest_key]
+            self.points = self.decrement(self.points, key)
+            self.points[self.MAX_ITEMS] = key
+        return self.cache_data.get(key)
+
+    def decrement(self, points: dict, key: str) -> dict:
+        temp = {}
+        key_pts = get_key_from_value(points, key)
+        for point, key_name in points.items():
+            if point <= self.MAX_ITEMS and point != key_pts:
+                temp[point - 1] = key_name
+                point -= 1
+            if point < self.mini:
+                self.mini = point
+
+        return temp
+
+    def decrement2(self, points: dict, key: str, ) -> dict:
+        temp = {}
+        key_point = get_key_from_value(points, key)
+        for point, key_name in points.items():
+
+            if point > key_point:
+                temp[point - 1] = key_name
+            else:
+                temp[point] = key_name
+
+            if point < self.mini:
+                self.mini = point
+
+        # del temp[key_point]
+        temp[self.MAX_ITEMS] = key
+
+        return temp
+
+
+if "__main__" == __name__:
+    my_cache = LRUCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    print(my_cache.get("B"))
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
+    my_cache.print_cache()
