30134842c9c55705576c66630ae8eb70fe5695bc
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-30 22:13:27 +0300

    Add Flask app with Babel for internationalization
    This commit introduces a simple Flask application configured with Babel for internationalization (i18n) support. The application includes a basic configuration for languages (English and French), sets the default timezone to UTC, and specifies English as the default locale. A locale selector function is implemented to determine the best match for the user's language preferences.
    
    The application's main route is defined to render a basic HTML template, "2-index.html", which displays a "Hello world" message. The template is set up with standard HTML5 structure and includes necessary meta tags for modern web compatibility.
    
    This setup lays the foundation for further development and customization of the application, particularly in enhancing its internationalization capabilities and expanding its feature set.

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+modules to run a simple flask app with babel translation
+"""
+from flask import request, Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Set default language configuration for language . """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def school():
+    """
+    This function is used to render a school page .
+    it is used for example in the example of the school module .
+
+    """
+    return render_template("2-index.html")
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
