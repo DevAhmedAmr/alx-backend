2344aa27d7f42bcc4598d2b47a560c368c79c226
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-30 23:57:19 +0300

    Refactor Flask app for internationalization with Babel
    Add Flask-Babel integration for internationalization (i18n) and localization (l10n) in the simple Flask app. Introduce a Config class to set default language configurations, including supported languages and default timezone. Implement a locale selector function to dynamically choose the locale based on request parameters or user preference. Enhance the app to render templates with i18n support, preparing it for multilingual content rendering.

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,52 @@
 #!/usr/bin/python3
+"""
+modules to run a simple flask app with babel translation
+"""
+from flask import request, Flask, render_template
+from flask_babel import Babel, gettext
+import logging
+log = logging
+# Set level to DEBUG to capture all levels of log messages
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class Config(object):
+    """Set default language configuration for language . """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Returns the locale for the current locale .
+
+    """
+    locals = request.args.get("locale")
+    print(request.args)
+
+    if locals in app.config["LANGUAGES"]:
+
+        return locals
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """
+    This function is used to render a school page .
+    it is used for example in the example of the school module .
+
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="50000", host="0.0.0.0", debug=True)
