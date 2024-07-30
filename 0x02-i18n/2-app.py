dadedc560b6546ed35e5ab7064c6190c1e4babb3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-30 22:32:44 +0300

    Refactor route function name to follow naming conventions
    Changed the function name from `school` to `index` in the route decorator to adhere to common naming conventions for the root endpoint in web applications. This change enhances readability and consistency with industry standards, making the codebase more intuitive for developers familiar with such practices.

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,7 @@ def get_locale():
 
 
 @app.route("/")
-def school():
+def index():
     """
     This function is used to render a school page .
     it is used for example in the example of the school module .
